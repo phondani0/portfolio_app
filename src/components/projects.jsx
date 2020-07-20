784668e39fcf5789b370954fab45386b6d556c7e
Ankit <ankitphondani52@gmail.com>
2020-07-20 18:03:06 +0530

    Updated prjects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,113 +4,113 @@ export default class Projects extends Component {
   render() {
     return (
       <div>
-				<section className="colorlib-work" data-section="projects">
-					<div className="colorlib-narrow-content">
-						<div className="row">
-							<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-								<span className="heading-meta">My Work</span>
-								<h2 className="colorlib-heading animate-box">Recent Projects</h2>
-							</div>
-						</div>
-						<div className="row">
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-								<div className="project" style={{backgroundImage: 'url(images/img-1.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 01</a></h3>
-											<span>Website</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-								<div className="project" style={{backgroundImage: 'url(images/img-2.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 02</a></h3>
-											<span>Animation</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
-								<div className="project" style={{backgroundImage: 'url(images/img-3.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 03</a></h3>
-											<span>Illustration</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInBottom">
-								<div className="project" style={{backgroundImage: 'url(images/img-4.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 04</a></h3>
-											<span>Application</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-								<div className="project" style={{backgroundImage: 'url(images/img-5.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 05</a></h3>
-											<span>Graphic, Logo</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-								<div className="project" style={{backgroundImage: 'url(images/img-6.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 06</a></h3>
-											<span>Web Design</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="row">
-							<div className="col-md-12 animate-box">
-								<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
-							</div>
-						</div>
-					</div>
-				</section>
+        <section className="colorlib-work" data-section="projects">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">My Work</span>
+                <h2 className="colorlib-heading animate-box">Recent Projects</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+                <div className="project" style={{ backgroundImage: 'url(images/img-1.jpg)' }}>
+                  <div className="desc">
+                    <div className="con">
+                      <h3><a href="work.html">Acme Web Design</a></h3>
+                      <span>AcmeWebDesign landing page made using Html, Css, Javascript, Bootstrap, Jquery, Animate.css</span>
+                      <p className="icon">
+                        <span><a href="https://phondani0.github.io/AcmeWebDesign/" rel="noopener noreferrer" target="_blank"><i className="icon-link" /> View</a></span>
+                        <span><a href="https://github.com/phondani0/AcmeWebDesign" rel="noopener noreferrer" target="_blank"><i className="icon-github" /> Github</a></span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+                <div className="project" style={{ backgroundImage: 'url(images/img-2.jpg)' }}>
+                  <div className="desc">
+                    <div className="con">
+                      <h3><a href="work.html">Github Finder</a></h3>
+                      <span>Find Github users and their latest repositories using github Api. Build uing Html, Css, Javascript, Bootstrap, Ajax</span>
+                      <p className="icon">
+                        <span><a href="https://phondani0.github.io/github-finder/" rel="noopener noreferrer" target="_blank"><i className="icon-link" /> View</a></span>
+                        <span><a href="https://github.com/phondani0/github-finder" rel="noopener noreferrer" target="_blank"><i className="icon-github" /> Github</a></span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
+                <div className="project" style={{ backgroundImage: 'url(images/img-3.jpg)' }}>
+                  <div className="desc">
+                    <div className="con">
+                      <h3><a href="work.html">HowToTech (Blogging Platform)</a></h3>
+                      <span>Blog MVC app made with Node.js, Express.js, MongoDB and Handlebars.js, Html, Css, Javascript, Bootstrap, Heroku</span>
+                      <p className="icon">
+                        <span><a href="https://howtotech-ap.herokuapp.com/" rel="noopener noreferrer" target="_blank"><i className="icon-link" /> View</a></span>
+                        <span><a href="https://github.com/phondani0/howtotech" rel="noopener noreferrer" target="_blank"><i className="icon-github" /> Github</a></span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 animate-box" data-animate-effect="fadeInBottom">
+                <div className="project" style={{ backgroundImage: 'url(images/img-4.jpg)' }}>
+                  <div className="desc">
+                    <div className="con">
+                      <h3><a href="work.html">Corona Dashboard</a></h3>
+                      <span>Dashboard for latest covid-19 stats. Build using React.js, Material UI and Fetch Api</span>
+                      <p className="icon">
+                        <span>
+                          <a href="https://phondani0.github.io/corona_dashboard/" rel="noopener noreferrer" target="_blank"><i className="icon-link" /> View</a></span>
+                        <span>
+                          <a href="https://github.com/phondani0/corona_dashboard" rel="noopener noreferrer" target="_blank"><i className="icon-github" /> Github</a>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+                <div className="project" style={{ backgroundImage: 'url(images/img-5.jpg)' }}>
+                  <div className="desc">
+                    <div className="con">
+                      <h3><a href="work.html">TripBazaar (Hotel booking platform)</a></h3>
+                      <span>MEAN Stack (MongoDb, Express.js, Angular, Node.js) and AWS</span>
+                      <p className="icon">
+                        <span><a href="https://tripbazaar.co" rel="noopener noreferrer" target="_blank">
+                          <i className="icon-link" /> View</a></span>
+                        {/* <span><a href="#"><i className="icon-github" /> Github</a></span> */}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+                <div className="project" style={{ backgroundImage: 'url(images/img-6.jpg)' }}>
+                  <div className="desc">
+                    <div className="con">
+                      <h3><a href="work.html">E-commerce Store</a></h3>
+                      <span>Ecommerce Store application build with Node.js, GraphQL, React.js, Redux, Prisma (ORM), PostgresSQL, and React-admin</span>
+                      <br />
+                      <h4>IN PROGRESS...</h4>
+                      <p className="icon">
+                        <span><a href="#"><i className="icon-link" /> View</a></span>
+                        <span><a href="https://github.com/phondani0/ecommerce_store" rel="noopener noreferrer" target="_blank"><i className="icon-github" /> Github</a></span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* <div className="row">
+              <div className="col-md-12 animate-box">
+                <p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
+              </div>
+            </div> */}
+          </div>
+        </section>
       </div>
     )
   }
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -17,9 +17,8 @@ export default class Sidebar extends Component {
                 <ul>
                   <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
                   <li><a href="#about" data-nav-section="about">About</a></li>
-                  {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
-                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
                   <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
+                  <li><a href="#projects" data-nav-section="projects">Projects</a></li>
                 </ul>
               </div>
             </nav>
@@ -39,7 +38,6 @@ export default class Sidebar extends Component {
                   Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration
               </small></p>
               <p><small>
-                Something coming soon !!
               </small></p>
             </div>
           </aside>
